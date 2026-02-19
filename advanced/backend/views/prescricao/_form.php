7f2563352e4e1a3c79705d623159f02615c11b41
Slenxe <2241574@my.ipleiria.pt>
2026-02-19 13:13:59 +0000

    .

--- advanced/backend/views/prescricao/_form.php
+++ advanced/backend/views/prescricao/_form.php
@@ -1,7 +1,6 @@
 <?php
 
 use kartik\select2\Select2;
-use kartik\widgets\Select2Asset;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,54 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fef834eeda91aee7eb0a49b430d896bfc1cc439e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fef834eeda91aee7eb0a49b430d896bfc1cc439e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'kartik-v/yii2-krajee-base' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5c095126d1be47e0bb1f92779b7dc099f6feae31',
+            'type' => 'yii2-extension',
+            'install_path' => __DIR__ . '/../kartik-v/yii2-krajee-base',
+            'aliases' => array(
+                0 => '3.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'kartik-v/yii2-widget-select2' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '958ba374af39fbc88f1fd11209b22efe1055d567',
+            'type' => 'yii2-extension',
+            'install_path' => __DIR__ . '/../kartik-v/yii2-widget-select2',
+            'aliases' => array(
+                0 => '2.2.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'select2/select2' => array(
+            'pretty_version' => '4.0.13',
+            'version' => '4.0.13.0',
+            'reference' => '45f2b83ceed5231afa7b3d5b12b58ad335edd82e',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../select2/select2',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
